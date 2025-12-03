ee74a96a0f3c2bbb0e478ed249b4720a59197a71
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2025-12-03 12:53:18 +0530

    Change escapeHTML function to be private

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export function escapeHTML(str: string): string {
+function escapeHTML(str: string): string {
   return str
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
